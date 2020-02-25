277be55ae5ea602740363b1f897341106c2f0321
maxjwebber <tua83483@temple.edu>
2020-02-25 18:38:18 -0500

    restyle

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main
 {
     public static void main(String[] args) {
-        System.out.print("I'm the original baby!");
+        System.out.println("I'm the original baby!");
+        System.out.println("Time to restyle!");
     }
 
 
